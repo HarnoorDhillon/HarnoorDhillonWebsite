1dbfb8038927fc8ea5888762c5fdee6e435a7411
Harnoor Dhillon <dhillon.h@berkeley.edu>
2023-07-22 18:32:30 -0700

    updated resume

--- src/Navbar.js
+++ src/Navbar.js
@@ -10,7 +10,7 @@ const NavBar = () => {
                 <li><Link activeClass="active" to="projects" spy={true} smooth={true} duration={500}>Projects</Link></li>
                 <li><Link activeClass="active" to="contact" spy={true} smooth={true} duration={500}>Contact</Link></li>
                 <li>
-                  <a className="resume-link" href="https://www.dropbox.com/s/uj8ej2xlcuxl6ky/Resume.pdf?dl=0" target="_blank" rel="noopener noreferrer">
+                  <a className="resume-link" href="https://www.dropbox.com/scl/fi/bxyym3gkb4hxatjy81b66/Resume.pdf?rlkey=uuvmy7tf6iob0lhoblgl12e4x&dl=0" target="_blank" rel="noopener noreferrer">
                     Resume <i class="fa-solid fa-arrow-down-to-line"></i>
                   </a>
                 </li>
